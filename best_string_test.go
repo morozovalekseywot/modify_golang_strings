e7775bb0200b6c193a7688e8fa012f51df96ebbc
morozovalekseywot <morozovalekseywot@gmail.com>
2021-12-30 14:51:14 +0300

    Initial commit

--- best_string_test.go
+++ best_string_test.go
@@ -19,18 +19,19 @@ func TestBestString(t *testing.T) {
 	bs.Set("123414*&()#@$%  \n\t  testify")
 	bs.ToUpperFirst()
 	require.Equal(t, bs.str, "123414*&()#@$%  \n\t  Testify")
-	require.True(t, bs.IsUpper(strings.Index(bs.str, "T")))
+	ans, _ := bs.IsUpper(strings.Index(bs.str, "T"))
+	require.True(t, ans)
 
 	bs.ToLowerFirst()
 	require.Equal(t, bs.str, "123414*&()#@$%  \n\t  testify")
-	require.True(t, bs.IsNumber(0))
-	require.True(t, bs.IsLower(strings.Index(bs.str, "t")))
+	ans, _ = bs.IsLower(strings.Index(bs.str, "t"))
+	require.True(t, ans)
 
 	bs.Set("\t\n  \t word \n\t")
 	bs.TrimSpace()
 	require.Equal(t, bs.str, "word")
 	require.Equal(t, bs.size, 4)
 
-	_,err := bs.IsDigit(4)
-	require.EqualError(t, err,"Index out of range, index: 4, string size: 4\n")
+	_, err := bs.IsDigit(4)
+	require.EqualError(t, err, "Index out of range, index: 4, string size: 4\n")
 }
